e133eaf704a667685a134714da3f88e873581fa2
Thomas Boerger <thomas@webhippie.de>
2022-10-23 01:27:25 +0200

    chore: drop openapi related template

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Nix configuration for Gopad Go";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    };
+
+    utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs = { self, nixpkgs, utils, ... }@inputs:
+    {
+
+    } // utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            buf
+            gnumake
+          ];
+        };
+      }
+    );
+}
